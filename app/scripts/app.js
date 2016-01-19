8d908fb13b2476ac7f1d453f81e8168d0601b5cc
Adel.Metzler <adel.metzler@senior.com.br>
2016-01-19 14:59:30 -0200

    com gulp funcionando

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,76 +1,75 @@
 'use strict';
 
 angular.module('confusionApp', [])
-.controller('menuController', function(){
-  this.tab= 1; //pagina 1 do index tab, vai ser a selecionada
-this.filtText='';
-
-  var dishes=[
+.controller('MenuController', ['$scope', function($scope) {
+  $scope.tab= 1; //pagina 1 do index tab, vai ser a selecionada
+  $scope.filtText='';
+  $scope.showDetails = false;
+  $scope.dishes=[
     {
-    name:'Uthapizza',
-    image: 'images/uthapizza.png',
-    category: 'mains',
-    label:'Hot',
-    price:'4.99',
-    description: 'A unique combinantion of many things, not sure just waiting to load the page',
-    comment: 'abc'
+      name:'Uthapizza',
+      image: 'images/uthapizza.png',
+      category: 'mains',
+      label:'Hot',
+      price:'4.99',
+      description: 'A unique combinantion of many things, not sure just waiting to load the page',
+      comment: 'abc'
 
     },
     {
-    name:'Zucchipakoda',
-    image: 'images/Zucchipakoda.png',
-    category: 'appetizer',
-    label: '',
-    price:'1.99',
-    description: 'A deep fried combinantion of many things',
-    comment: 'abc'
+      name:'Zucchipakoda',
+      image: 'images/Zucchipakoda.png',
+      category: 'appetizer',
+      label: '',
+      price:'1.99',
+      description: 'A deep fried combinantion of many things',
+      comment: 'abc'
 
     },
     {
-    name:'Vadonut',
-    image: 'images/Vadonut.png',
-    category: 'appetizer',
-    label:'New',
-    price:'1.99',
-    description: 'A quintessense  fried combinantion of many things',
-    comment: 'abc'
+      name:'Vadonut',
+      image: 'images/Vadonut.png',
+      category: 'appetizer',
+      label:'New',
+      price:'1.99',
+      description: 'A quintessense  fried combinantion of many things',
+      comment: 'abc'
 
     },
     {
-    name:'ElaiCheese Cake',
-    image: 'images/elaicheesecake.png',
-    category: 'dessert',
-    label:'New',
-    price:'1.99',
-    description: 'A quintessense  fried combinantion of many things',
+      name:'ElaiCheese Cake',
+      image: 'images/elaicheesecake.png',
+      category: 'dessert',
+      label:'New',
+      price:'1.99',
+      description: 'A quintessense  fried combinantion of many things',
 
     }
 
 
-    ];
+  ];
 
-  this.dishes = dishes;
-
-
-
-  this.select = function(setTab){
-    this.tab=setTab;
+  $scope.select = function(setTab){
+    $scope.tab=setTab;
 
 
-  if(setTab===2) {
-    this.filtText="appetizer";
+    if(setTab===2) {
+      $scope.filtText="appetizer";
     }
-  else if (setTab===3){
-    this.filtText='mains';
+    else if (setTab===3){
+      $scope.filtText='mains';
     }
-  else if (setTab===4){
-    this.filtText="dessert";
+    else if (setTab===4){
+      $scope.filtText="dessert";
     }
-  else{
-    this.filtText="";
-  }};
+    else{
+      $scope.filtText="";
+    }};
 
-  this.isSelected =function(checkTab){return (this.tab===checkTab);
-  };
+    $scope.isSelected =function(checkTab){return ($scope.tab===checkTab);
+    };
+$scope.toggleDetails = function(){
+  $scope.showDetails = !$scope.showDetails;
+};
 
-});
+  }]);
